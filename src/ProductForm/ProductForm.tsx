7e4c8e21f8c8e009b53d8715e59c14781a69cf01
alannguyen5102 <alannguyen5102@gmail.com>
2020-10-15 18:30:29 -0700

    Taking out the FormRow div class

--- src/ProductForm/ProductForm.tsx
+++ src/ProductForm/ProductForm.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import FormRow from '../FormRow/FormRow';
 import WireGauge from '../WireGauge/WireGauge';
 
 export default class ProductForm extends React.Component {
     render() {
         return (
             <div className="form-inline">
-                <WireGauge></WireGauge>
+                <FormRow>
+                    <WireGauge></WireGauge>
+                </FormRow>
+                
             </div>
             
         );
--- src/WireGauge/WireGauge.tsx
+++ src/WireGauge/WireGauge.tsx
@@ -17,18 +17,18 @@ export default class WireGauge extends React.Component<IProps, IState> {
 
   render() {
     return (
-        <div className="form-row">
-            <div className="input-group">
-                <label className="input-group-addon" htmlFor="wire-gauge-box">
-                        Wire Gauge*
-                    </label>
-                <select className="form-control" value={this.state.value} onChange={this.handleChange}>            <option value="grapefruit">Grapefruit</option>
-                    <option value="lime">Lime</option>
-                    <option value="coconut">Coconut</option>
-                    <option value="mango">Mango</option>
-                </select>
-            </div>
+        
+        <div className="input-group">
+            <label className="input-group-addon" htmlFor="wire-gauge-box">
+                    Wire Gauge*
+                </label>
+            <select className="form-control" value={this.state.value} onChange={this.handleChange}>            <option value="grapefruit">Grapefruit</option>
+                <option value="lime">Lime</option>
+                <option value="coconut">Coconut</option>
+                <option value="mango">Mango</option>
+            </select>
         </div>
+
 
     );
   }
