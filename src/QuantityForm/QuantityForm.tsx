6492fe6ffd561c58275805087c2352a380c8a0cf
alannguyen5102 <alannguyen5102@gmail.com>
2020-10-19 16:36:17 -0700

    Adding InputGroups

--- src/QuantityForm/QuantityForm.tsx
+++ src/QuantityForm/QuantityForm.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import InputGroup from '../InputGroup/InputGroup';
 import LabelAddOn from '../LabelAddOn/LabelAddOn';
 import './QuantityForm.scss';
 
 export default class QuantityForm extends React.Component {
     render() {
         return (
-            <div className="input-group">
+            <InputGroup>
                 <LabelAddOn htmlFor="quantity-box">Quantity*</LabelAddOn>
                 <input className="form-control" type="number"></input>
-            </div>
+            </InputGroup>
         );
     }
 }
--- src/WireGauge/WireGauge.tsx
+++ src/WireGauge/WireGauge.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import InputGroup from '../InputGroup/InputGroup';
 import LabelAddOn from '../LabelAddOn/LabelAddOn';
 interface IProps {
 }
@@ -19,14 +20,14 @@ export default class WireGauge extends React.Component<IProps, IState> {
   render() {
     return (
         
-        <div className="input-group">
+        <InputGroup>
             <LabelAddOn htmlFor="wire-gauge-box">Wire Gauge*</LabelAddOn>
             <select className="form-control" value={this.state.value} onChange={this.handleChange}>            <option value="grapefruit">Grapefruit</option>
                 <option value="lime">Lime</option>
                 <option value="coconut">Coconut</option>
                 <option value="mango">Mango</option>
             </select>
-        </div>
+        </InputGroup>
 
 
     );
