ffe4f3d0dfafbc8dc8f24d61d50a96f3cc7ac633
alannguyen5102 <alannguyen5102@gmail.com>
2020-10-19 16:48:12 -0700

    Adding SubmitButton

--- src/ProductForm/ProductForm.tsx
+++ src/ProductForm/ProductForm.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import FormRow from '../FormRow/FormRow';
 import QuantityForm from '../QuantityForm/QuantityForm';
 import SectionLabel from '../SectionLabel/SectionLabel';
+import SubmitButton from '../SubmitButton/SubmitButton';
 import WireGauge from '../WireGauge/WireGauge';
 
 export default class ProductForm extends React.Component {
@@ -16,6 +17,9 @@ export default class ProductForm extends React.Component {
                 </FormRow>
                 <FormRow>
                     <SectionLabel>Feedthrough</SectionLabel>
+                </FormRow>
+                <FormRow>
+                    <SubmitButton>Request a Quote</SubmitButton>
                 </FormRow>
             </div>
             
