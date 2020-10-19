8cababc6bc944edbf11a3cb3472af3aa7e6e13f5
alannguyen5102 <alannguyen5102@gmail.com>
2020-10-19 16:35:58 -0700

    testing SectionLabel

--- src/ProductForm/ProductForm.tsx
+++ src/ProductForm/ProductForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FormRow from '../FormRow/FormRow';
 import QuantityForm from '../QuantityForm/QuantityForm';
+import SectionLabel from '../SectionLabel/SectionLabel';
 import WireGauge from '../WireGauge/WireGauge';
 
 export default class ProductForm extends React.Component {
@@ -13,7 +14,9 @@ export default class ProductForm extends React.Component {
                 <FormRow>
                     <WireGauge></WireGauge>
                 </FormRow>
-                
+                <FormRow>
+                    <SectionLabel>Feedthrough</SectionLabel>
+                </FormRow>
             </div>
             
         );
