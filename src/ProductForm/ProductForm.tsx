0844a2d1f5060e7f437aa63287f6bb8b8f2e31b6
alannguyen5102 <alannguyen5102@gmail.com>
2020-10-19 14:51:02 -0700

    updating ProductForm with Quantity and FormRow

--- src/ProductForm/ProductForm.tsx
+++ src/ProductForm/ProductForm.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import FormRow from '../FormRow/FormRow';
+import QuantityForm from '../QuantityForm/QuantityForm';
 import WireGauge from '../WireGauge/WireGauge';
 
 export default class ProductForm extends React.Component {
     render() {
         return (
             <div className="form-inline">
+                <FormRow>
+                    <QuantityForm></QuantityForm>
+                </FormRow>
                 <FormRow>
                     <WireGauge></WireGauge>
                 </FormRow>
