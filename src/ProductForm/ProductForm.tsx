078534951d84aa19e776e08b41b2ea8c43130a73
alannguyen5102 <alannguyen5102@gmail.com>
2020-10-19 17:28:15 -0700

    Adding in Disclaimers

--- src/ProductForm/ProductForm.tsx
+++ src/ProductForm/ProductForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import DisclaimerText from '../DisclaimerText/DisclaimerText';
 import FormRow from '../FormRow/FormRow';
 import QuantityForm from '../QuantityForm/QuantityForm';
 import SectionLabel from '../SectionLabel/SectionLabel';
@@ -9,6 +10,16 @@ export default class ProductForm extends React.Component {
     render() {
         return (
             <div className="form-inline">
+                <DisclaimerText>
+                    * = Required Field
+                </DisclaimerText>
+                <DisclaimerText>
+                    † All vacuum feedthroughs are leak tested 10 <sub>-7</sub> to 10 <sub>-9</sub>atm-cc/sec 
+                </DisclaimerText>
+                <DisclaimerText>
+                    †† Dimensions are show in inches.
+                </DisclaimerText>
+
                 <FormRow>
                     <QuantityForm></QuantityForm>
                 </FormRow>
@@ -18,6 +29,16 @@ export default class ProductForm extends React.Component {
                 <FormRow>
                     <SectionLabel>Feedthrough</SectionLabel>
                 </FormRow>
+                
+                <DisclaimerText>
+                    ††† Visual is intended for reference and not to scale.
+                </DisclaimerText>
+                <DisclaimerText>
+                    This application is intended to simplify the feedthrough request process for the average user.  
+                </DisclaimerText>
+                <DisclaimerText>
+                    If you require something that is not currently available with the given selections, please feel free to give us the details in the application notes below so we can better assist you.
+                </DisclaimerText>
                 <FormRow>
                     <SubmitButton>Request a Quote</SubmitButton>
                 </FormRow>
